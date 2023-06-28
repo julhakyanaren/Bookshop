1dd141dbed5decb1637ba687d640ab1c54361688
ArenJulhakyan <mgc.julhakyan@gmail.com>
2023-06-28 16:42:51 +0400

    Добавьте файлы проекта.

---
+++ Draw.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Security;
+
+namespace Bookshop
+{
+    public static class Colorspace
+    {
+        public static class Colors_Default
+        {
+            public static class FormCS
+            {
+                public static Color ForeColor = SystemColors.Control;
+                public static Color BackColor = SystemColors.ControlDark;
+                public static Color[] ColorSpace = { ForeColor, BackColor };
+            }
+            public static class ButtonCS
+            {
+                public static Color ForeColor = SystemColors.Control;
+                public static Color BackColor = SystemColors.ControlDark;
+                public static Color[] ColorSpace = { ForeColor, BackColor };
+            }
+            public static class DataTimePickerCS
+            {
+                public static Color ForeColor = SystemColors.Control;
+                public static Color MonthBackground = SystemColors.ControlDark;
+                public static Color TitleForeColor = SystemColors.ActiveCaptionText;    //Заглавия календаря
+                public static Color TitleBackColor = SystemColors.ActiveCaption;
+                public static Color TrailingForeColor = SystemColors.GrayText;          //Начальные и конечные даты
+                public static Color[] ColorSpace = { ForeColor, MonthBackground, TitleForeColor, TitleForeColor, TrailingForeColor};
+            }
+            public static Color[][] DefaultColorspace = { FormCS.ColorSpace, ButtonCS.ColorSpace, DataTimePickerCS.ColorSpace };
+        }
+        public static class Colors_Dark
+        {
+            public static class FormCS
+            {
+                public static Color ForeColor = SystemColors.ControlDark;
+                public static Color BackColor = SystemColors.Control;
+                public static Color[] ColorSpace = { ForeColor, BackColor };
+            }
+            public static class ButtonCS
+            {
+                public static Color ForeColor = SystemColors.ControlDark;
+                public static Color BackColor = SystemColors.Control;
+                public static Color[] ColorSpace = { ForeColor, BackColor };
+            }
+            public static class DataTimePickerCS
+            {
+                public static Color ForeColor = SystemColors.ControlDark;
+                public static Color MonthBackground = SystemColors.Control;
+                public static Color TitleForeColor = Color.DodgerBlue;    //Заглавия календаря
+                public static Color TitleBackColor = Color.White;
+                public static Color TrailingForeColor = Color.Blue;       //Начальные и конечные даты
+                public static Color[] ColorSpace = { ForeColor, MonthBackground, TitleForeColor, TitleForeColor, TrailingForeColor };
+            }
+            public static Color[][] DarkColorspace = { FormCS.ColorSpace, ButtonCS.ColorSpace, DataTimePickerCS.ColorSpace };
+        }
+        public static class Colors_OrangeBlack
+        {
+            public static class FormCS
+            {
+                public static Color ForeColor = Color.Black;
+                public static Color BackColor = Color.DarkOrange;
+                public static Color[] ColorSpace = { ForeColor, BackColor };
+            }
+            public static class ButtonCS
+            {
+                public static Color ForeColor = Color.Black;
+                public static Color BackColor = Color.DarkOrange;
+                public static Color[] ColorSpace = { ForeColor, BackColor };
+            }
+            public static class DataTimePickerCS
+            {
+                public static Color ForeColor = Color.DarkOrange;
+                public static Color MonthBackground = Color.Black;
+                public static Color TitleForeColor = Color.SandyBrown;    //Заглавия календаря
+                public static Color TitleBackColor = Color.Black;
+                public static Color TrailingForeColor = Color.OrangeRed;       //Начальные и конечные даты
+                public static Color[] ColorSpace = { ForeColor, MonthBackground, TitleForeColor, TitleForeColor, TrailingForeColor };
+            }
+            public static Color[][] OrangeBlackColorspace = { FormCS.ColorSpace, ButtonCS.ColorSpace, DataTimePickerCS.ColorSpace };
+        }
+        public static Color[][][] ColorspaceArray = { Colors_Default.DefaultColorspace, Colors_Dark.DarkColorspace, Colors_OrangeBlack.OrangeBlackColorspace };
+    }
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Bookshop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(59, 49);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 749);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
